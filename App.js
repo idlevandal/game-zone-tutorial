2e3507d81a6146e061fd5a6f52011ec609b92e39
David Allen <david@inthecode.com.au>
2020-03-10 12:56:12 +1000

    intitial commit, finished #18

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+import {AppLoading} from 'expo';
+
+const getFonts = () => Font.loadAsync({
+  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+});
+
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
+    return (
+        <Home />
+    );
+  } else {
+    return (
+        <AppLoading
+            startAsync={getFonts}
+            onFinish={() => setFontsLoaded(true)}
+        />
+    )
+  }
+}
